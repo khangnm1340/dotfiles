1cdb3fdf5ca084630a4ebbcff353d60d4ba86b10
pampam <khangnm1340@ut.edu.vn>
2025-09-12 22:13:54 +0700

    fixed the multicursor freeze

--- pamsdots/dot-config/nvim-hanni/init.lua
+++ pamsdots/dot-config/nvim-hanni/init.lua
@@ -9,6 +9,7 @@ vim.o.ignorecase     = true
 vim.o.smartcase      = true
 vim.o.number         = false
 vim.o.relativenumber = false
+vim.opt.expandtab    = true
 vim.o.wrap           = false
 vim.o.tabstop        = 4
 vim.o.shiftwidth     = 4
@@ -31,15 +32,15 @@ vim.keymap.set("n", "<M-[>", ":tabprevious<CR>", { noremap = true, silent = true
 vim.keymap.set("n", "<C-t>", ":tabnew<CR>", { noremap = true, silent = true })
 vim.keymap.set("t", "<S-Esc>", [[<C-\><C-n>]])
 vim.keymap.set('n', '<leader>n',
-	"<cmd>new | setlocal buftype=nofile bufhidden=wipe noswapfile | 0put =execute('silent messages')<CR>",
-	{ silent = true, desc = 'Open :messages in scratch buffer' }
+    "<cmd>new | setlocal buftype=nofile bufhidden=wipe noswapfile | 0put =execute('silent messages')<CR>",
+    { silent = true, desc = 'Open :messages in scratch buffer' }
 )
 
 -- Normal mode keymap: <leader>cp to copy parent directory
 vim.keymap.set("n", "<C-z>", function()
-	local parent_dir = vim.fn.expand("%:p:h")
-	vim.fn.setreg("+", parent_dir) -- copy to system clipboard (+ register)
-	vim.notify("Copied: " .. parent_dir)
+    local parent_dir = vim.fn.expand("%:p:h")
+    vim.fn.setreg("+", parent_dir) -- copy to system clipboard (+ register)
+    vim.notify("Copied: " .. parent_dir)
 end, { desc = "Copy parent directory of current file" })
 local map = vim.keymap.set
 map({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
@@ -62,27 +63,26 @@ map('n', 'gs', ':se spell!<CR>', { silent = true })
 map("n", "<leader>t", ":lua require('toggle-checkbox').toggle()<CR>", { silent = true, desc = "Toggle CheckBox" })
 map("n", "<leader>l", ":LazyGit<CR>", { silent = true })
 map("n", "<C-e>", function()
-	local session = vim.fn.expand("~/.local/share/nvim-hanni/session.vim")
-	vim.cmd("source " .. session)
+    local session = vim.fn.expand("~/.local/share/nvim-hanni/session.vim")
+    vim.cmd("source " .. session)
 end, { desc = "Restore last session" })
-
 
 --- }}}
 
 -- 3) Packages {{{
 vim.pack.add({
-	{ src = "https://github.com/nvim-mini/mini.nvim" },
-	{ src = "https://github.com/norcalli/nvim-colorizer.lua" },
-	{ src = "https://github.com/folke/flash.nvim" },
-	{ src = "https://github.com/mbbill/undotree" },
-	{ src = "https://github.com/neovim/nvim-lspconfig" },
-	{ src = "https://github.com/kdheepak/lazygit.nvim" },
-	{ src = "https://github.com/nvim-lua/plenary.nvim" },
-	{ src = "https://github.com/mason-org/mason.nvim" },
-	{ src = "https://github.com/folke/tokyonight.nvim" },
-	{ src = "https://github.com/mikavilpas/yazi.nvim" },
-	{ src = "https://github.com/opdavies/toggle-checkbox.nvim" },
-	{ src = "https://github.com/jake-stewart/multicursor.nvim" },
+    { src = "https://github.com/nvim-mini/mini.nvim" },
+    { src = "https://github.com/norcalli/nvim-colorizer.lua" },
+    { src = "https://github.com/folke/flash.nvim" },
+    { src = "https://github.com/mbbill/undotree" },
+    { src = "https://github.com/neovim/nvim-lspconfig" },
+    { src = "https://github.com/kdheepak/lazygit.nvim" },
+    { src = "https://github.com/nvim-lua/plenary.nvim" },
+    { src = "https://github.com/mason-org/mason.nvim" },
+    { src = "https://github.com/folke/tokyonight.nvim" },
+    { src = "https://github.com/mikavilpas/yazi.nvim" },
+    { src = "https://github.com/opdavies/toggle-checkbox.nvim" },
+    { src = "https://github.com/jake-stewart/multicursor.nvim" },
 })
 -- }}}
 
@@ -97,15 +97,15 @@ require("mason").setup()
 require("mini.pick").setup()
 require("mini.ai").setup()
 require("mini.surround").setup({
-	mappings = {
-		add = 'gsa',   -- Add surrounding
-		delete = 'gsd', -- Delete surrounding
-		replace = 'gsr', -- Replace surrounding
-		find = 'gsf',  -- Find surrounding
-		find_left = 'gsF', -- your custom find (left)
-		highlight = 'gsh', -- Highlight surrounding
-		update_n_lines = '', -- disable updating lines by key
-	}
+    mappings = {
+        add = 'gsa',         -- Add surrounding
+        delete = 'gsd',      -- Delete surrounding
+        replace = 'gsr',     -- Replace surrounding
+        find = 'gsf',        -- Find surrounding
+        find_left = 'gsF',   -- your custom find (left)
+        highlight = 'gsh',   -- Highlight surrounding
+        update_n_lines = '', -- disable updating lines by key
+    }
 })
 require("mini.pairs").setup()
 ---@diagnostic disable-next-line: assign-type-mismatch, need-check-nil
@@ -160,62 +160,56 @@ mc.addKeymapLayer(function(layerSet)
 end)
 
 
+
+
 MiniDeps.add({
-	source = "saghen/blink.cmp",
-	depends = { "rafamadriz/friendly-snippets" },
-	checkout = "v1.6.0", -- check releases for latest tag
+    source = "saghen/blink.cmp",
+    depends = { "rafamadriz/friendly-snippets" },
+    checkout = "v1.6.0", -- check releases for latest tag
 })
+
 
 require('blink.cmp').setup({
-	-- mappings: 'default' ~ built-in-ish; try 'super-tab' or 'enter' if you prefer
-	keymap = { preset = 'default' },
-	appearance = { nerd_font_variant = 'mono' },
-	-- docs popup only when you ask (C-Space toggles docs if menu is open)
-	completion = { documentation = { auto_show = false } },
-	-- sources enabled by default; keep them explicit so you can extend later
-	sources = { default = { 'lsp', 'path', 'snippets', 'buffer' } },
-	-- just fuzzy matcher is fastest; fall back to Lua if the binary isn’t available
-	fuzzy = { implementation = 'prefer_rust_with_warning' },
+    -- mappings: 'default' ~ built-in-ish; try 'super-tab' or 'enter' if you prefer
+    keymap = { preset = 'default' },
+    appearance = { nerd_font_variant = 'mono' },
+    -- docs popup only when you ask (C-Space toggles docs if menu is open)
+    completion = { documentation = { auto_show = false } },
+    -- sources enabled by default; keep them explicit so you can extend later
+    sources = { default = { 'lsp', 'path', 'snippets', 'buffer' } },
+    -- just fuzzy matcher is fastest; fall back to Lua if the binary isn’t available
+    fuzzy = { implementation = 'prefer_rust_with_warning' },
 
-	enabled = function()
-		return not (ok_mc and mc.cursorsEnabled and mc.cursorsEnabled())
-	end,
+    -- enabled = function()
+    --  return not (ok_mc and mc.cursorsEnabled and mc.cursorsEnabled())
+    -- end,
 
 })
--- require("typst-preview").setup({
--- 	open_cmd = "qutebrowser --target window %s",
--- 	follow_cursor = false, -- no auto scroll on every motion
--- })
-
--- require("simple-zoom").setup({
--- 	hide_tabline = true,
--- })
-
 local flash = require("flash")
 flash.setup({
-	labels = "jkluioprewtfsgvmbq",
-	highlight = {
-		-- backdrop = false,
-	},
-	modes = {
-		char = {
-			jump_labels = true,
-			label = { exclude = "o" },
-			multi_line = true,
-			highlight = {
-				backdrop = true,
-			},
-		},
-	},
+    labels = "jkluioprewtfsgvmbq",
+    highlight = {
+        -- backdrop = false,
+    },
+    modes = {
+        char = {
+            jump_labels = true,
+            label = { exclude = "o" },
+            multi_line = true,
+            highlight = {
+                backdrop = true,
+            },
+        },
+    },
 })
 
 -- keymaps
 map({ "n", "x", "o" }, "s", function()
-	flash.jump()
+    flash.jump()
 end, { desc = "Flash" })
 
 map({ "n", "x", "o" }, "S", function()
-	flash.treesitter({ labels = "jkluiohprewtfsgvmcbq" })
+    flash.treesitter({ labels = "jkluiohprewtfsgvmcbq" })
 end, { desc = "Flash Treesitter" })
 
 -- }}}
@@ -223,39 +217,39 @@ end, { desc = "Flash Treesitter" })
 -- 5) LSP & Diagnostics {{{
 
 MiniDeps.add({
-	source = "nvim-treesitter/nvim-treesitter",
-	-- nvim-treesitter uses rolling releases; no tag needed.
-	-- Run TSUpdate on install/updates so parsers get compiled.
-	hooks  = {
-		post_install  = function() vim.cmd("TSUpdate") end,
-		post_checkout = function() vim.cmd("TSUpdate") end,
-	},
+    source = "nvim-treesitter/nvim-treesitter",
+    -- nvim-treesitter uses rolling releases; no tag needed.
+    -- Run TSUpdate on install/updates so parsers get compiled.
+    hooks  = {
+        post_install  = function() vim.cmd("TSUpdate") end,
+        post_checkout = function() vim.cmd("TSUpdate") end,
+    },
 })
 require("nvim-treesitter.configs").setup({
-	-- pick your languages
-	ensure_installed = {
-		"lua", "vim", "vimdoc", "query",
-		"bash", "python", "json", "yaml", "toml",
-		"markdown", "markdown_inline",
-		"regex", "c", "rust", "typst", "nu",
-	},
-	auto_install = true, -- auto install missing parsers
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = false, -- keep it purely TS
-	},
-	indent = { enable = true },              -- Treesitter-based indent (some langs opt-out)
-	incremental_selection = {
-		enable = true,
-		keymaps = {
-			init_selection    = "<CR>", -- start selection
-			node_incremental  = "<CR>", -- expand
-			node_decremental  = "<BS>", -- shrink
-			scope_incremental = "<Tab>",
-		},
-	},
-	-- If you add the textobjects plugin later, you can enable its modules here.
-	-- textobjects = { select = { enable = true, keymaps = { ["af"]="@function.outer", ["if"]="@function.inner" } } }
+    -- pick your languages
+    ensure_installed = {
+        "lua", "vim", "vimdoc", "query",
+        "bash", "python", "json", "yaml", "toml",
+        "markdown", "markdown_inline",
+        "regex", "c", "rust", "typst", "nu",
+    },
+    auto_install = true, -- auto install missing parsers
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false, -- keep it purely TS
+    },
+    indent = { enable = true },                    -- Treesitter-based indent (some langs opt-out)
+    incremental_selection = {
+        enable = true,
+        keymaps = {
+            init_selection    = "<CR>", -- start selection
+            node_incremental  = "<CR>", -- expand
+            node_decremental  = "<BS>", -- shrink
+            scope_incremental = "<Tab>",
+        },
+    },
+    -- If you add the textobjects plugin later, you can enable its modules here.
+    -- textobjects = { select = { enable = true, keymaps = { ["af"]="@function.outer", ["if"]="@function.inner" } } }
 })
 
 vim.lsp.enable({ "emmylua_ls", "tinymist" })
@@ -264,11 +258,11 @@ vim.lsp.enable({ "emmylua_ls", "tinymist" })
 -- vim.diagnostic.enable(false)
 
 vim.lsp.config("tinymist", {
-	cmd = { "tinymist" }, -- keep it simple; lspconfig/Mason can still override PATH
-	filetypes = { "typst" }, -- optional, but nice to be explicit
-	settings = {
-		formatterMode = "typstyle"
-	},
+    cmd = { "tinymist" },    -- keep it simple; lspconfig/Mason can still override PATH
+    filetypes = { "typst" }, -- optional, but nice to be explicit
+    settings = {
+        formatterMode = "typstyle"
+    },
 })
 
 -- }}}
@@ -281,28 +275,28 @@ map("n", "<leader>g", ":Pick grep<CR>", { desc = "Pick grep" })
 
 -- Frequent files from Nushell history
 map(
-	"n",
-	"<M-k>",
-	":lua require('mini.pick').start({source={items=vim.fn.readfile(vim.fn.expand('/home/pampam/.config/nushell/pams_history.txt'))}, choose=function(p) vim.cmd.edit(vim.fn.fnameescape(vim.fn.expand(p))) end})<CR>",
-	{ desc = "Pick frequent files", silent = true }
+    "n",
+    "<M-k>",
+    ":lua require('mini.pick').start({source={items=vim.fn.readfile(vim.fn.expand('/home/pampam/.config/nushell/pams_history.txt'))}, choose=function(p) vim.cmd.edit(vim.fn.fnameescape(vim.fn.expand(p))) end})<CR>",
+    { desc = "Pick frequent files", silent = true }
 )
 
 MiniPick.registry.files = function(local_opts)
-	local opts = { source = { cwd = local_opts.cwd } }
-	local_opts.cwd = nil
-	return MiniPick.builtin.files(local_opts, opts)
+    local opts = { source = { cwd = local_opts.cwd } }
+    local_opts.cwd = nil
+    return MiniPick.builtin.files(local_opts, opts)
 end
 
 map("n", "<leader>j", function()
-	require("mini.pick").registry.files({
-		cwd = vim.fn.expand("%:p:h"),
-	})
+    require("mini.pick").registry.files({
+        cwd = vim.fn.expand("%:p:h"),
+    })
 end, { desc = "Pick files from buffer dir" })
 -- }}}
 
 -- 7) UI & Colors {{{
 require("tokyonight").setup({
-  transparent = not vim.g.neovide,
+    transparent = not vim.g.neovide,
 })
 vim.cmd("colorscheme tokyonight")
 vim.cmd(":hi statusline guibg=NONE")
@@ -311,12 +305,12 @@ vim.cmd(":hi statusline guibg=NONE")
 -- 8) Neovide {{{
 ---@diagnostic disable-next-line: undefined-field
 if vim.g.neovide then
-	map({ "n", "v" }, "<M-u>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>",
-		{ desc = "Increase font size", silent = true })
-	map({ "n", "v" }, "<M-U>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>",
-		{ desc = "Decrease font size", silent = true })
-	map({ "n", "v" }, "<M-C-u>", ":lua vim.g.neovide_scale_factor = 1<CR>", { desc = "Reset font size", silent = true })
-	map("c", "<C-S-v>", "<C-R>+", { desc = "Paste in command mode" })
+    map({ "n", "v" }, "<M-u>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>",
+        { desc = "Increase font size", silent = true })
+    map({ "n", "v" }, "<M-U>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>",
+        { desc = "Decrease font size", silent = true })
+    map({ "n", "v" }, "<M-C-u>", ":lua vim.g.neovide_scale_factor = 1<CR>", { desc = "Reset font size", silent = true })
+    map("c", "<C-S-v>", "<C-R>+", { desc = "Paste in command mode" })
 end
 vim.g.neovide_cursor_animation_length = 0.1
 
@@ -326,31 +320,32 @@ vim.g.neovide_normal_opacity = 0.85
 
 -- 9) Autocmds {{{
 vim.api.nvim_create_autocmd("TextYankPost", {
-	group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank({ higroup = "IncSearch", timeout = 150 })
-	end,
+    group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }),
+    callback = function()
+        vim.highlight.on_yank({ higroup = "IncSearch", timeout = 150 })
+    end,
 })
 
 vim.api.nvim_create_autocmd("BufReadPost", {
-	pattern = "/tmp/*",
-	callback = function()
-		vim.cmd("normal! G")
-	end,
+    pattern = "/tmp/*",
+    callback = function()
+        vim.cmd("normal! G")
+    end,
 })
 vim.api.nvim_create_autocmd("VimLeavePre", {
-	callback = function()
-		vim.cmd("mksession! ~/.local/share/nvim-hanni/session.vim")
-	end,
+    callback = function()
+        vim.cmd("mksession! ~/.local/share/nvim-hanni/session.vim")
+    end,
 })
 vim.api.nvim_create_autocmd("SessionLoadPost", {
-	callback = function()
-		for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
-			if vim.api.nvim_buf_is_loaded(bufnr) then
-				pcall(vim.lsp.document_color.enable, false, bufnr)
-			end
-		end
-	end,
+    callback = function()
+        for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
+            if vim.api.nvim_buf_is_loaded(bufnr) then
+                pcall(vim.lsp.document_color.enable, false, bufnr)
+            end
+        end
+    end,
 })
 
 -- }}}
+
